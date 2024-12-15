1cbc750c8775377db57bea21a7b7c1e2ddd419d8
jax880811 <jax880811@gmail.com>
2024-12-15 22:32:52 +0800

    邏輯回歸修正

--- 2024 12 14邏輯迴歸.py
+++ 2024 12 14邏輯迴歸.py
@@ -39,7 +39,6 @@ disp = ConfusionMatrixDisplay.from_estimator(logit, X_test, y_test,
                                              cmap=plt.cm.Blues)
 plt.grid()
 plt.show()
-print(logit.score(X_test,y_test))
 y_pred = logit.predict(X_test)
 print(classification_report(y_test, y_pred))
 test_score = logit.score(X_test, y_test) * 100

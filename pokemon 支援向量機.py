b48c115b3688f97ae1e7eb9db7fbea0a90b1352f
jax880811 <jax880811@gmail.com>
2024-12-17 22:00:16 +0800

    pokemon svm 練習

---
+++ pokemon 支援向量機.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import ConfusionMatrixDisplay, classification_report
+from sklearn.svm import LinearSVC
+
+df=pd.read_csv('Pokemon.csv', encoding='cp1252',header=0)
+
+df['hasType2'] = df['Type_2'].notnull().astype(int)
+
+X, y = df.loc[:, 'HP':'Speed'], df['hasType2']
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+scale = StandardScaler().fit(X_train)
+X_train_std = scale.transform(X_train)
+X_test_std = scale.transform(X_test)
+Linear_svm = LinearSVC(max_iter=1500)
+Linear_svm.fit(X_train_std, y_train)
+
+y_pred = Linear_svm.predict(X_test_std)
+print(classification_report(y_test, y_pred))
+
